2d3fc0b9846a200d1a95fc960e0791d633ec7f7d
Seif Waleed <seif30570@gmail.com>
2026-07-17 14:08:45 +0300

    Delete server directory

--- server/src/routes/placeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getPlaces, getPlaceById } = require('../controllers/placeController');
-
-const router = express.Router();
-
-router.get('/', getPlaces);
-router.get('/:id', getPlaceById);
-
-module.exports = router;
--- server/src/utils/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-/**
- * Wraps an async route handler and forwards any errors to Express's
- * error-handling middleware, avoiding repetitive try/catch blocks.
- */
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = asyncHandler;
